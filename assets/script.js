f40b5e96b116db34e4617347fe130c1bff8b7679
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2023-12-03 23:01:13 -0500

    added event listeners to buttons, ready to add quiz logic

--- assets/script.js
+++ assets/script.js
@@ -9,10 +9,10 @@ var cardTitle = document.querySelector("#questionNumber");
 var showQuestion = document.querySelector("#theQuestion");
 var showChoices = document.querySelector("#theChoices");
 var checkAnswer = document.querySelector("#answerCheck");
-var answer_one = document.querySelector("#answer_one");
-var answer_two = document.querySelector("#answer_two");
-var answer_three = document.querySelector("#answer_three");
-var answer_four = document.querySelector("#answer_four");
+var choiceButton1 = document.querySelector("#answerBtn1");
+var choiceButton2 = document.querySelector("#answerBtn2");
+var choiceButton3 = document.querySelector("#answerBtn3");
+var choiceButton4 = document.querySelector("#answerBtn4");
 
 // Question Cards
 var question_one = "What is the answer to this question?";
@@ -23,64 +23,31 @@ var question_five = "What is the answer to this question?";
 var allQuestions = [question_one, question_two, question_three, question_four, question_five];
 
 //Answer Key
-var qcOne = {
-    A: true,
-    B: false, 
-    C: false, 
-    D: false
-}
-
-var qcTwo = {
-    A: false,
-    B: true, 
-    C: false, 
-    D: false
-}
-
-var qcThree = {
-    A: false,
-    B: false, 
-    C: true, 
-    D: false
-}
-
-var qcFour = {
-    A: true,
-    B: false, 
-    C: false, 
-    D: false
-}
-
-var qcFive = {
-    A: false,
-    B: false, 
-    C: false, 
-    D: true
-}
-correctAnswers = [qcOne.A, qcTwo.B, qcThree.C, qcFour.A, qcFive.D];
-
-
 
 //start the quiz - show question 1
 start.addEventListener("click", function(){
     quizStart.setAttribute("style", "display:none;");
     questionnaire.setAttribute("style", "display:inline;");
     cardTitle.textContent = "Question 1";
-    showChoices.textContent = "A.) This is the answer. B.) This is incorrect. C.) This is incorrect D.) This is incorrect";
     showQuestion.textContent = allQuestions[0];
 });
 
-// Question 1
-answerBtn.addEventListener("click", function(){
-    if (interactiveBtn === true ) {
-        checkAnswer.textContent = "Correct!"
-    } else{
-        checkAnswer.textContent = "Incorrect!"
-    }    
+// Buttons initialized
+choiceButton1.addEventListener("click", function(event){
+    console.log(event.target) 
+})
+choiceButton2.addEventListener("click", function(event){
+    console.log(event.target) 
+})
+choiceButton3.addEventListener("click", function(event){
+    console.log(event.target) 
+})
+choiceButton4.addEventListener("click", function(event){
+    console.log(event.target) 
 })
 
+// Question 1
 // Question 2
-
 // Question 3
 // Question 4
 // Question 5
